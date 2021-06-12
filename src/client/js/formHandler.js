0cde0d3ac6c611d9055fb327e991e2a059dc8fdc
Abd-ElRahman007 <abdelrhman123work@gmail.com>
2021-06-12 10:52:14 +0200

    making the jest test

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -12,15 +12,24 @@ function handleSubmit(event) {
             try {
                 const fullLink = await link.json();
                 console.log(fullLink);
-                let textInfo = '';
-                // const container = document.createDocumentFragment();
-                const bodyInfo = document.getElementById('results').innerHTML = `agreement:${fullLink.agreement},<br>
-            model:${fullLink.model},<br>
-            confidence:${fullLink.confidence},<br>
-            irony;${fullLink.irony}`;
-                // const body = document.getElementById('results');
-                // container.appendChild(bodyInfo);
-                // body.appendChild(container);
+                let formText = '';
+                for (const i of fullLink.sentence_list) {
+                    const text = fullLink.sentence_list[i];
+                    console.log(text);
+                    formText = `${formText} ${text}`;
+                    return formText;
+                }
+                const container = document.createDocumentFragment();
+                const ui = document.createElement('div');
+                ui.innerHTML = `agreement:${fullLink.agreement},<br>
+                model:${fullLink.model},<br>
+                confidence:${fullLink.confidence},<br>
+                irony:${fullLink.irony},<br>
+                text:${formText}`;
+                container.appendChild(ui);
+                const result = document.querySelector('#results');
+                result.appendChild(container);
+                console.log(container.firstChild);
                 return fullLink;
             } catch (error) {
                 console.log('error', error);
